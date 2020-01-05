dbf184a89c81942915f18bceb25c645db39981a3
Andrew Obiano <andrew.obiano@gmail.com>
2020-01-04 15:54:24 -1000

    finished

--- secret_number.js
+++ secret_number.js
@@ -1,11 +1,12 @@
-'use strict';
+"use strict";
 module.exports = function() {
-    let answer = []; // the empty box
-    return function secretNumber(){
-        if(answer.length === 0){ // only if the box is empty
-            // only then, we'll assign ONE random number to it
-            return answer = Math.floor(Math.random() * 1000001);
-        }
-    return answer; // need to give the answer
+  let answer = null; // the empty box
+  return function secretNumber() {
+    if (answer === null) {
+      // only if the answer is empty
+      // only then, we'll assign ONE random number to it
+      return (answer = Math.floor(Math.random() * 1000001));
     }
+    return answer; // need to give the answer
+  };
 };
